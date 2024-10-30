817a964b7713bc0b9c7091c417e4dda6c6b94e6d
Noel Saludo <noelsaludo17@gmail.com>
2024-10-30 16:47:53 +0800

    first commit

---
+++ app/src/main/java/FinalProject/App.java
@@ -0,0 +1,14 @@
+package FinalProject; 
+
+import java.util.Scanner;
+
+public class App {
+    Scanner in = new Scanner(System.in);
+    App() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
